e9b07c94fafc1833f15ce0aaffcc8478dc8f3202
Filipe Carvalho <filipe.silva@trf1.jus.br>
2024-08-29 15:18:41 -0300

    reref: criação do crud do projeto

--- src/main/java/com/unb/fga/calorieexplorer/controller/UsuarioController.java
+++ src/main/java/com/unb/fga/calorieexplorer/controller/UsuarioController.java
@@ -2,15 +2,15 @@ package com.unb.fga.calorieexplorer.controller;
 
 import com.unb.fga.calorieexplorer.model.UsuarioEntity;
 import com.unb.fga.calorieexplorer.service.UsuarioService;
+import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@RequestMapping("/calorie-counter")
+@RequestMapping("/api")
 @RestController
 public class UsuarioController {
     @Autowired
@@ -21,4 +21,20 @@ public class UsuarioController {
        return usuarioService.getAllUsuarios();
     }
 
+    @GetMapping("/usuarios/{id}")
+    public Optional<UsuarioEntity> getUsuarioById(@PathVariable("id") Integer id){
+        return usuarioService.getUsuarioById(id);
+    }
+
+    @PostMapping("/usuarios")
+    public ResponseEntity<UsuarioEntity> createUsuario(@RequestBody UsuarioEntity usuario){
+        UsuarioEntity user = usuarioService.createUsuario(usuario);
+        return ResponseEntity.ok().body(user);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    public ResponseEntity<UsuarioEntity> deleteUsuarioById(@PathVariable("id") Integer id){
+        usuarioService.deleteUsuario(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/unb/fga/calorieexplorer/service/UsuarioService.java
+++ src/main/java/com/unb/fga/calorieexplorer/service/UsuarioService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -14,6 +15,22 @@ public class UsuarioService {
 
     public List<UsuarioEntity> getAllUsuarios() {
         return usuarioRepository.findAll();
+    }
+
+    public Optional<UsuarioEntity> getUsuarioById(Integer id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public UsuarioEntity createUsuario(UsuarioEntity usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deleteUsuario(Integer id) {
+        Optional<UsuarioEntity> idUsuario = usuarioRepository.findById(id);
+        if (idUsuario.isEmpty()) {
+            throw new RuntimeException("usuario nao encontrado");
+        }
+        usuarioRepository.deleteById(id);
     }
 }
 
