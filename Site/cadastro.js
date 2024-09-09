611f31ffacb14deacd45cbbe4645b39e272cf23f
AJCGassassin <andrezinhojoao3@gmail.com>
2024-09-09 17:24:31 -0300

    tentando fazer funcionar

--- Site/cadastro.js
+++ Site/cadastro.js
@@ -1,40 +1,41 @@
-const formulario = document.querySelector("form");
+const formulario = document.querySelector("#formulario");
 const inome = document.querySelector(".nome");
 const iemail = document.querySelector(".email");
 const itelefone = document.querySelector(".telefone");
 const isenha = document.querySelector(".senha");
-const botao = document.querySelector("Button");
+const botao = document.querySelector("button");
 
-function cadastrar(){
-
-    fetch("http://localhost:8080/cadastro",
-        {
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json"
-            },
-            method : "POST",
-            body: JSON.Stringify({
-                nome: inome.value,
-                email: iemail.value,
-                senha: isenha.value,
-                telefone: itelefone.value
-            })
+function cadastrar() {
+    fetch("http://localhost:3306/api/usuarios", {
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            nome: inome.value,
+            email: iemail.value,
+            senha: isenha.value,
+            telefone: itelefone.value
         })
-
-    .then(function (res) {conseole.log(res) })
-    .catch(function (res) {console.lgo(res) })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("Usuário criado com sucesso:", data);
+    })
+    .catch(error => {
+        console.error("Erro ao criar usuário:", error);
+    });
 }
 
-function limpar (){
-    inome.value = "",
-    iemail.value = "",
-    isenha.value = "",
-    itelefone.value = ""
-
+function limpar() {
+    inome.value = "";
+    iemail.value = "";
+    isenha.value = "";
+    itelefone.value = "";
 }
 
-formulario.addEventListener("submit", function(event){
+formulario.addEventListener("submit", function(event) {
     event.preventDefault();
     cadastrar();
     limpar();
